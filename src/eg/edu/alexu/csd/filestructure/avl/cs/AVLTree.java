d364b8df026ed1f9b6ad60c6f047ad458bd6bd5f
Abdelhakeem Osama <abdelhakeem.osama@hotmail.com>
2018-03-20 15:41:42 +0200

    Add insertBelow and implement insert

--- src/eg/edu/alexu/csd/filestructure/avl/cs/AVLTree.java
+++ src/eg/edu/alexu/csd/filestructure/avl/cs/AVLTree.java
@@ -8,9 +8,113 @@ public class AVLTree<T extends Comparable<T>> implements IAVLTree<T> {
     private Node<T> root;
     private int size = 0;
 
+    /**
+     * Inserts the given node below the given parent node,
+     * maintaining the AVL tree property.
+     *
+     * @param node inserted node
+     * @param parent parent node
+     * @param grandParent grandparent node, null if parent is root
+     * @param parentSide relation of parent node to grandParent node.
+     * Can be -1 or 1 depending on whether it is the left child or
+     * the right child, respectively
+     *
+     * @return -1 or 1 indicating whether the node has been inserted
+     * to the left or to the right, respectively
+     */
+    private int insertBelow(
+            final INode<T> node,
+            final INode<T> parent,
+            final INode<T> grandParent,
+            final int parentSide) {
+        if (parent == null) {
+            throw new IllegalArgumentException("Parent should NOT be null");
+        }
+
+        if (node == null) {
+            throw new IllegalArgumentException("Nothing to insert");
+        }
+
+        Node<T> parentNode = (Node<T>) parent;
+        Node<T> grandParentNode = (Node<T>) grandParent;
+        Node<T> newParent = parentNode;
+        int side = -1;
+
+        if (node.getValue().compareTo(parentNode.getValue()) > 0) {
+            side = 1;
+            Node<T> rightChild = (Node<T>) parentNode.getRightChild();
+            if (rightChild == null) {
+                parentNode.setRightChild(node);
+            } else {
+                int result = insertBelow(node, rightChild, parentNode, 1);
+                int rightHeight = rightChild.getHeight();
+                int leftHeight = -1;
+                Node<T> leftChild = (Node<T>) parentNode.getLeftChild();
+
+                if (leftChild != null) {
+                    leftHeight = leftChild.getHeight();
+                }
+
+                if (rightHeight - leftHeight > 1) {
+                    if (result == 1) {
+                        newParent = rotateLeft(parentNode);
+                    } else {
+                        newParent = doubleRotationRightLeft(parentNode);
+                    }
+                }
+            }
+        } else {
+            Node<T> leftChild = (Node<T>) parentNode.getLeftChild();
+            if (leftChild == null) {
+                parentNode.setLeftChild(node);
+            } else {
+                int result = insertBelow(node, leftChild, parentNode, -1);
+                int leftHeight = leftChild.getHeight();
+                int rightHeight = -1;
+                Node<T> rightChild = (Node<T>) parentNode.getRightChild();
+
+                if (rightChild != null) {
+                    rightHeight = rightChild.getHeight();
+                }
+
+                if (leftHeight - rightHeight > 1) {
+                    if (result == -1) {
+                        newParent = rotateRight(parentNode);
+                    } else {
+                        newParent = doubleRotationLeftRight(parentNode);
+                    }
+                }
+            }
+        }
+
+        if (grandParentNode == null) {
+            this.root = newParent;
+        } else {
+            if (parentSide == -1) {
+                grandParentNode.setLeftChild(newParent);
+            } else {
+                grandParentNode.setRightChild(newParent);
+            }
+        }
+
+        parentNode.updateHeight();
+        return side;
+    }
+
+    /**
+     * Inserts a new node with the given key in the tree.
+     *
+     * @param key key of new node
+     */
     @Override
-    public void insert(T key) {
-        // TODO Abdelhakeem
+    public final void insert(final T key) {
+        if (root == null) {
+            root = new Node<T>(key);
+        } else {
+            insertBelow(new Node<T>(key), root, null, 0);
+        }
+
+        size += 1;
     }
 
     @Override
